d86f32ee48e311221f031bfd160eba07201d895b
mounikar_backbase <mounikar@backbase.com>
2026-06-14 14:29:20 +0530

    skeleton of office cateria app

---
+++ src/main/java/com/example/OfficeCafeteriaMenu/OfficeCafeteriaMenuApplication.java
@@ -0,0 +1,13 @@
+package com.example.OfficeCafeteriaMenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficeCafeteriaMenuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficeCafeteriaMenuApplication.class, args);
+	}
+
+}
